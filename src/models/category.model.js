8aad19cb31c17faaa660fdf53985449b1c0c14b6
Nawal Saw <mksaw19@gmail.com>
2024-08-02 13:22:07 +0530

    completed product controller

--- src/models/category.model.js
+++ src/models/category.model.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
 
 const categorySchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true , lowercase: true},
   parent_id: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Category",
     default: null,
   },
-  ancestors: [{ type: mongoose.Schema.Types.ObjectId, ref: "Category" }],
 });
 
 export const Category = mongoose.model("Category", categorySchema);
